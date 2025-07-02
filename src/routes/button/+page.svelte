4c341e096347b2242d3e887f07884b55cbd95c7e
wuuski <97132315+wuuski@users.noreply.github.com>
2025-07-02 14:22:30 -0700

    test test test teste

--- src/routes/button/+page.svelte
+++ src/routes/button/+page.svelte
@@ -2,7 +2,6 @@
     let count1 = $state(16);
     const count1Clamped = $derived(Math.min(64, count1))
     let count2 = $state(16);
-
     const count2Clamped = $derived(Math.min(64, count2))
 </script>
 
@@ -15,7 +14,7 @@
     </div>
     
     <div class="bg-red-300 size-64 rounded-full grid justify-center items-center">
-        <button onclick={() => count2 = Math.min(64, count2 + 1)} class="bg-amber-800 text-white rounded-full size-7" style="width: {count2Clamped}px; height: {count2Clamped}px;">
+        <button onclick={() => count2 += 1} class="bg-amber-800 text-white rounded-full size-7" style="width: {count2Clamped}px; height: {count2Clamped}px;">
             {count2}
         </button>
     </div>
